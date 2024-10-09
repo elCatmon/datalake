15f86957e2ef2b8b85041233b7cdc58fc1ea91bc
Cesar Ortega <89800127+elCatmon@users.noreply.github.com>
2024-10-09 17:01:25 -0600

    update

--- services/service.go
+++ services/service.go
@@ -251,6 +251,11 @@ func ProcesarDonacionFisica(w http.ResponseWriter, r *http.Request) ([]interface
 		http.Error(w, err.Error(), http.StatusBadRequest)
 	}
 
+	region, err := getValueOrError(formData.Value, "region")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+
 	sexo, err := getValueOrError(formData.Value, "sexo")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
@@ -288,7 +293,7 @@ func ProcesarDonacionFisica(w http.ResponseWriter, r *http.Request) ([]interface
 		return nil, errors.New("no anonymized files uploaded")
 	}
 
-	datos := []interface{}{estudioID, donador, estudio, hash, "00", "0", sexo, edad, anonymizedFiles, originalFiles}
+	datos := []interface{}{estudioID, donador, estudio, hash, region, "0", sexo, edad, anonymizedFiles, originalFiles}
 
 	return datos, err
 }
@@ -525,8 +530,8 @@ func BuscarImagenEstudioNombre(imagenNombre string, db *mongo.Database) (primiti
 	return fileDoc.ID, nil
 }
 
-// BuscarDiagnosticoReciente busca el diagnóstico más reciente de un estudio dado su _id
-func BuscarDiagnosticoReciente(ctx context.Context, db *mongo.Database, id primitive.ObjectID) (*models.Diagnostico, error) {
+// BuscarDiagnosticoReciente busca el diagnóstico más reciente de un estudio dado su _id y el nombre de la imagen
+func BuscarDiagnosticoReciente(ctx context.Context, db *mongo.Database, id primitive.ObjectID, nombreImagen string) (*models.Diagnostico, string, error) {
 	// Definir la colección
 	collection := db.Collection("estudios")
 
@@ -536,14 +541,14 @@ func BuscarDiagnosticoReciente(ctx context.Context, db *mongo.Database, id primi
 	err := collection.FindOne(ctx, filter).Decode(&estudio)
 	if err != nil {
 		if err == mongo.ErrNoDocuments {
-			return nil, errors.New("no se encontró el documento con el ID proporcionado")
+			return nil, "", errors.New("no se encontró el documento con el ID proporcionado")
 		}
-		return nil, err
+		return nil, "", err
 	}
 
 	// Si no tiene diagnósticos, regresar un error
 	if len(estudio.Diagnostico) == 0 {
-		return nil, errors.New("el estudio no tiene diagnósticos")
+		return nil, "", errors.New("el estudio no tiene diagnósticos")
 	}
 
 	// Encontrar el diagnóstico más reciente
@@ -554,5 +559,46 @@ func BuscarDiagnosticoReciente(ctx context.Context, db *mongo.Database, id primi
 		}
 	}
 
-	return &diagnosticoReciente, nil
+	// Obtener la clave de la imagen basada en el nombre de la imagen
+	clave, err := GetImageKeyByFileName(nombreImagen, db)
+	if err != nil {
+		return nil, "", err
+	}
+
+	return &diagnosticoReciente, clave, nil
+}
+
+// Función para buscar la clave de una imagen por su nombre en GridFS.
+func GetImageKeyByFileName(filename string, db *mongo.Database) (string, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// Buscar el archivo en GridFS.
+	var fileDoc models.FileDocument
+	err := db.Collection("imagenes.files").FindOne(ctx, bson.M{"filename": filename}).Decode(&fileDoc)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return "", errors.New("archivo no encontrado en GridFS")
+		}
+		return "", fmt.Errorf("error buscando el archivo en GridFS: %v", err)
+	}
+
+	// Buscar en la colección `estudios` usando el ID del archivo.
+	var estudioDoc models.EstudioDocument
+	err = db.Collection("estudios").FindOne(ctx, bson.M{"imagenes.dicom": fileDoc.ID.Hex()}).Decode(&estudioDoc)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return "", errors.New("no se encontró referencia en la colección de estudios")
+		}
+		return "", fmt.Errorf("error buscando en la colección de estudios: %v", err)
+	}
+
+	// Recorrer las imágenes para encontrar la clave correspondiente.
+	for _, imagen := range estudioDoc.Imagenes {
+		if imagen.Dicom == fileDoc.ID.Hex() { // Verificar si el DICOM coincide con el ID del archivo en formato hexadecimal.
+			return imagen.Clave, nil
+		}
+	}
+
+	return "", errors.New("clave no encontrada para la imagen")
 }
